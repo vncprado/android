2800f098edac8b74f99a2e8b755b81900ba2e0e7
Vinicius Prado da Fonseca <vncprado@gmail.com>
2016-06-06 18:12:39 -0400

    Icons, app menu tests and explicit intent.

--- IntentApplication/app/src/main/java/com/example/vinicius/intentapplication/MainActivity.java
+++ IntentApplication/app/src/main/java/com/example/vinicius/intentapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vinicius.intentapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,9 +46,30 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_refresh) {
+            Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
             return true;
         }
 
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonExplicitIntentOnClick(View v){
+        Intent explicitIntent = new Intent(this, ExplicitActivity.class);
+        startActivity(explicitIntent);
+    }
+
+    public void buttonImplicitIntentOnClick(View v){
+        String textMessage = "TESTE";
+        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage);
+        sendIntent.setType("text/plain");
+
+        if (sendIntent.resolveActivity(getPackageManager()) != null){
+            startActivity(sendIntent);
+        }
     }
 }
